1877dd0d470db49ee49cb5bcfd3185447b0430cf
Fabian-c-h <chogofabian64@gmail.com>
2025-02-24 02:13:58 -0600

    Update chogo compound.c

--- chogo compound.c
+++ chogo compound.c
@@ -1,7 +1,7 @@
 // Programm to find the compound interest 
 /*
-STUDENT NAME: RAYMOND NDORO 
-ADM NUMBER: D33-2402-2023
+STUDENT NAME: FABIAN CHOGO
+ADM NUMBER: D33-0262-2022
     
     */
     #include <stdio.h>
